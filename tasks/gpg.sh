5573403821df3a207a4fefc210e9dfaf8ac82773
stivius <maxim11853@gmail.com>
2022-09-17 15:29:51 +0300

    Automatic backup 17-09-2022 15:29:51

--- tasks/gpg.sh
+++ tasks/gpg.sh
@@ -4,10 +4,8 @@ sudo apt install -y gpg
 
 pushd $HOME
 
-gpg --import master-pub.gpg
-gpg --import master-sec.gpg
-gpg --import subkeys-pub.gpg
-gpg --import subkeys-sec.gpg
+gpg --import public.asc
+gpg --import private.asc
 
 popd
 
